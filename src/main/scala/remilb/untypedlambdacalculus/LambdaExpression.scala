86db8f2e01bac75120dcaba10016e57a413cfae3
Ryan Bauer <ryanemilbauer@gmail.com>
2019-11-11 13:29:57 -0800

    Small tweak

--- src/main/scala/remilb/untypedlambdacalculus/LambdaExpression.scala
+++ src/main/scala/remilb/untypedlambdacalculus/LambdaExpression.scala
@@ -1,33 +1,31 @@
 package remilb.untypedlambdacalculus
 
-sealed abstract class LambdaExpression {
+sealed abstract class LambdaExpression
 
-}
-
-case class Var(x:Char) extends LambdaExpression {
+case class Var(x: Char) extends LambdaExpression {
   override def toString = x.toString
 }
 
-case class Lambda(x:Var,e:LambdaExpression) extends LambdaExpression {
-  def apply(x:LambdaExpression) = Apply(this,x)
-  override def toString = "\\" + x + "." + e
+case class Lambda(x: Var, e: LambdaExpression) extends LambdaExpression {
+  def apply(x: LambdaExpression) = Apply(this, x)
+  override def toString = "\\" + x + " -> " + e
 }
 
-case class Apply(e1:LambdaExpression, e2:LambdaExpression) extends LambdaExpression {
+case class Apply(e1: LambdaExpression, e2: LambdaExpression) extends LambdaExpression {
   override def toString = "(" + e1.toString + " " + e2.toString + ")"
 }
 
 /** Case classes for Nameless remilb.untypedlambdacalculus.Lambda Expressions*/
 sealed abstract class NLE
 
-case class NVar(x:Int) extends NLE {
+case class NVar(x: Int) extends NLE {
   override def toString = x.toString
 }
 
-case class NLambda(e:NLE) extends NLE {
+case class NLambda(e: NLE) extends NLE {
   override def toString = "\\" + "." + e
 }
 
-case class NApply(e1:NLE, e2:NLE) extends NLE {
+case class NApply(e1: NLE, e2: NLE) extends NLE {
   override def toString = "(" + e1.toString + " " + e2.toString + ")"
 }
--- src/main/scala/remilb/untypedlambdacalculus/UntypedLambdaCalculusParser.scala
+++ src/main/scala/remilb/untypedlambdacalculus/UntypedLambdaCalculusParser.scala
@@ -1,16 +1,16 @@
 package remilb.untypedlambdacalculus
 
-import scala.util.parsing.combinator.{JavaTokenParsers, PackratParsers}
+import scala.util.parsing.combinator.{RegexParsers, PackratParsers}
 
 /**
- * Parser Combinator class for parsing remilb.untypedlambdacalculus.Lambda expression strings into ASTs of remilb.untypedlambdacalculus.Lambda Expressions
- * Grammar implemented more or less as per Pierce 5-3, with addition of parentheses for diambiguation. Applications
- * are left binding. Chapter 33 of Scala book on Parser Combinators used to figure out implementation. Use of
- * PackratParsers trait prevents blowing the stack through infinite left recursion, discovered through tip on StackOverflow
+ * Parser Combinator class for parsing string representations of untyped lambda calculus expressions into ASTs of
+ * LambdaExpressions. Grammar implemented more or less as per Pierce 5-3, with addition of parentheses for
+ * disambiguation. Application is left associative.
  */
-class UntypedLambdaCalculusParser extends JavaTokenParsers with PackratParsers{
+class UntypedLambdaCalculusParser extends RegexParsers with PackratParsers{
+  // Lexer rules
   lazy val variable: PackratParser[Var] = """[a-z]""".r ^^ {x => Var(x.charAt(0))}
-  lazy val abstraction: PackratParser[Lambda] = "\\"~>variable~"."~term ^^ {case boundVar~"."~body => Lambda(boundVar, body)}
+  lazy val abstraction: PackratParser[Lambda] = "\\"~>variable~"-"~term ^^ {case boundVar~"."~body => Lambda(boundVar, body)}
   lazy val application: PackratParser[Apply] = term~(abstraction | variable | term) ^^ {case term1~term2 => Apply(term1, term2)}
   lazy val parensTerm: PackratParser[LambdaExpression] = "(" ~> term <~ ")" ^^ {term => term}
   lazy val term: PackratParser[LambdaExpression] =  abstraction | application | variable | parensTerm
